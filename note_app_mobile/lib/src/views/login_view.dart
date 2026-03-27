e4a948298057f4e269dbcb912932b15b63b44f68
23120327 <thaithienphu74109630@gmail.com>
2026-03-27 15:59:34 +0700

    feat: login view

---
+++ note_app_mobile/lib/src/views/login_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import 'home_view.dart';
+
+class LoginView extends StatefuleWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final AuthService _authService = AuthService();
+  bool _isLoading = false;
+
+  void _login() async {
+    setState(() => _isLoading = true);
+
+    final result = await _authService.login(
+      _emailController.text,
+      _passwordController.text,
+    );
+
+    setState(() => _isLoading = false);
+
+    if (result['success']){
+      Navigator.pushReplacement(
+        context, 
+        MaterialPageRoute(builder: (context) => const HomeView())
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(result['message']), backgroundColor: Colors.red),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Đăng nhập Note App")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Ô nhập Email
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Ô nhập Password
+            TextField(
+              controller: _passwordController,
+              obscureText: true, // Để ẩn mật khẩu
+              decoration: const InputDecoration(
+                labelText: "Mật khẩu",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+            ),
+            const SizedBox(height: 30),
+            // Nút bấm hoặc Vòng xoay loading
+            _isLoading 
+              ? const CircularProgressIndicator() 
+              : ElevatedButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 50)),
+                  child: const Text("ĐĂNG NHẬP"),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
